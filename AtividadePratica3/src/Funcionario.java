9f4c95ee2baa16c6aff74d72259fed16f72b721e
Odair Oliveira <juniorodair6@gmail.com>
2024-05-01 12:21:02 -0300

    Adicionado classe abstrata Funcionario.java

---
+++ AtividadePratica3/src/Funcionario.java
@@ -0,0 +1,33 @@
+public abstract class Funcionario {
+    
+    private String nome;
+    private int matricula;
+
+    public Funcionario() {
+    }
+
+    public Funcionario(String nome, int matricula) {
+        this.nome = nome;
+        this.matricula = matricula;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(int matricula) {
+        this.matricula = matricula;
+    }
+
+    public abstract float calcularSalario();
+
+    
+}
